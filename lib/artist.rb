3abc3e0f047e8e45e98126995d483145c37e927c
Jonah Brown <jswish02@gmail.com>
2019-09-27 19:17:33 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,24 @@
+class Artist 
+  attr_accessor :name 
+  @@all = []
+  def initialize(name)
+    @name = name
+  @@all << self
+end
+def self.all
+  @@all 
+end
+def add_song(song)
+  song.artist= self
+end
+def add_song_name(name)
+  song = song.new(name)
+  song.artist = self
+end
+def songs 
+  song.all.all {|song| song.artist == self}
+end
+def self.song_count
+  song.all.count
+end
+end
